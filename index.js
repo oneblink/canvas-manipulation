e9bc5451cf8191d6147825c053faaabb68621e70
Matt Carroll <matt.carroll139@gmail.com>
2017-02-09 15:19:14 +1100

    drawImage, resize and toDataURL functions

---
+++ index.js
@@ -0,0 +1,12 @@
+/* @flow */
+'use strict'
+
+import drawImageCentered from './lib/draw-image-centered.js'
+import resize from './lib/resize.js'
+import toDataURLCropped from './lib/to-data-url-cropped.js'
+
+export {
+  drawImageCentered,
+  resize,
+  toDataURLCropped
+}
